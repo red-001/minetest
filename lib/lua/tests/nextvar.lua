e9ecdb190f963102af983c5e0f34d03cf18c9802
red-001 <red-001@outlook.ie>
2024-09-08 18:08:02 +0100

    Backport __ipairs and __pairs internal test from Lua 5.2

--- lib/lua/tests/nextvar.lua
+++ lib/lua/tests/nextvar.lua
@@ -393,4 +393,45 @@ for n,a,b,c,d in f(5,3) do
 end
 assert(x == 5)
 
+-- testing __pairs and __ipairs metamethod
+a = {}
+do
+  local x,y,z = pairs(a)
+  assert(type(x) == 'function' and y == a and z == nil)
+end
+
+local function foo (e,i)
+  assert(e == a)
+  if i <= 10 then return i+1, i+2 end
+end
+
+local function foo1 (e,i)
+  i = i + 1
+  assert(e == a)
+  if i <= e.n then return i,a[i] end
+end
+
+setmetatable(a, {__pairs = function (x) return foo, x, 0 end})
+
+local i = 0
+for k,v in pairs(a) do
+  i = i + 1
+  assert(k == i and v == k+1)
+end
+
+a.n = 5
+a[3] = 30
+
+a = {n=10}
+setmetatable(a, {__len = function (x) return x.n end,
+                 __ipairs = function (x) return function (e,i)
+                             if i < #e then return i+1 end
+                           end, x, 0 end})
+i = 0
+for k,v in ipairs(a) do
+  i = i + 1
+  assert(k == i and v == nil)
+end
+assert(i == a.n)
+
 print"OK"
